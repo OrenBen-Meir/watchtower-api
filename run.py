cd5018fbc0dc48c872cacaf7b206a43285570e3e
Oren Ben-Meir <orenbm11@gmail.com>
2020-07-03 10:11:41 -0400

    test commit for deployment

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>Hello WatchTower!</h1><br></body></html>\n"
+    return "<html><body><h1>Hello WatchTower!</h1><br><br><h1>Bottom text</h1></body></html>\n"
 
 if __name__ == '__main__':
     app.run()
