fa4edaff094065594650245595a3525ef4a1f349
Oren Ben-Meir <orenbm11@gmail.com>
2020-07-03 11:32:31 -0400

    changed launcher for deployment testing

---
+++ run.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<html><body><h1>Hello WatchTower!</h1><br><h2>Test azure change</h2><br><h1>Bottom text</h1></body></html>\n"
+
+if __name__ == '__main__':
+    app.run()
+ 
