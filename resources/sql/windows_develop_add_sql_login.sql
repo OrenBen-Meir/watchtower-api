d1f6f2206a6432f8680d753bd87641e593a6096a
Oren Ben-Meir <46640034+OrenBen-Meir@users.noreply.github.com>
2020-07-17 22:24:07 -0400

    Update windows_develop_add_sql_login.sql

--- resources/sql/windows_develop_add_sql_login.sql
+++ resources/sql/windows_develop_add_sql_login.sql
@@ -1,3 +1,4 @@
+-- for development on a windows machine, to be used for a localdb, not needed for linux and mac users because docker is used
 CREATE LOGIN myuser WITH PASSWORD = 'Password123'
 GO
 CREATE USER myuser FOR LOGIN myuser
