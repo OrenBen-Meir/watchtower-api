112082f161b3a1c09b60d53163fc0ab3f11f86c5
Oren Ben-Meir <orenbm11@gmail.com>
2020-07-23 00:18:03 -0400

    removed uneeded script

--- resources/sql/windows_develop_add_sql_login.sql
+++
@@ -1,7 +0,0 @@
--- for development on a windows machine, to be used for a localdb, not needed for linux and mac users because docker is used
-CREATE LOGIN myuser WITH PASSWORD = 'Password123'
-GO
-CREATE USER myuser FOR LOGIN myuser
-GO
-EXEC sp_addrolemember 'db_owner', 'myuser'
-GO
